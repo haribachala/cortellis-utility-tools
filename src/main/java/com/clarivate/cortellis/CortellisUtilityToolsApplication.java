8cb029d3bc3e473e367f7e6051858f279b7587e7
hariprasad.bachala <hariprasad.bachala@thomsonreuters.com>
2017-01-30 15:57:22 +0530

    replacing properties file with YAML file.

--- src/main/java/com/clarivate/cortellis/CortellisUtilityToolsApplication.java
+++ src/main/java/com/clarivate/cortellis/CortellisUtilityToolsApplication.java
@@ -1,12 +1,34 @@
 package com.clarivate.cortellis;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerPropertiesAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class CortellisUtilityToolsApplication {
+@Import(value={ServerPropertiesAutoConfiguration.class,
+		EmbeddedServletContainerAutoConfiguration.class,
+		WebMvcAutoConfiguration.class,
+		DispatcherServletAutoConfiguration.class,
+		PropertyPlaceholderAutoConfiguration.class
 
-	public static void main(String[] args) {
-		SpringApplication.run(CortellisUtilityToolsApplication.class, args);
+		})
+@EnableScheduling
+public class CortellisUtilityToolsApplication extends SpringBootServletInitializer {
+   public static void main(String[] args) {
+		//SpringApplication.run(CortellisUtilityToolsApplication.class, args);
+         new CortellisUtilityToolsApplication().configure(
+                new SpringApplicationBuilder(CortellisUtilityToolsApplication.class)).run(args);
 	}
-}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        application.sources(CortellisUtilityToolsApplication.class);
+        return application;
+    }
+   }
